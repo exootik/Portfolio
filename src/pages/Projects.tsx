dd723663668f10c37d9612f06a2b92b51b41ffdb
Exootik44 <71789229+exootik@users.noreply.github.com>
2026-05-05 10:04:46 +0200

    [ADD] proj AngryBird + Framed

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -4,7 +4,9 @@ import ProjectCard from '../components/ProjectCard'
 // Import images et videos
 // shaderZelda
 import framedPoster from '../assets/images/imgFramed.png';
-import framedVideo  from '../assets/videos/trailerFramed.mp4';
+//import framedVideo  from '../assets/videos/trailerFramed.mp4';
+import framedVideo  from '../assets/videos/videoAngryBirdLike.mp4';
+
 // angryBirdLike
 import angryBirdLikePoster from '../assets/images/imgAngryBirdLike.jpg';
 import angryBirdLikeVideo  from '../assets/videos/videoAngryBirdLike.mp4';
